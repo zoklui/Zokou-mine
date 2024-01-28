05caf9d32c837ea46ba546cc17059080f1887819
zoklui <154670714+zoklui@users.noreply.github.com>
2024-01-28 18:30:27 +0000

    Update img.js

--- commandes/img.js
+++ commandes/img.js
@@ -24,7 +24,7 @@ async (dest, zk, commandeOptions) => {
 
   function envoiImage(e,r)
    {
-      
+        if(e){repondre("oups une erreur ")}else{zk.sendM(dest,{image:{url:r.url[a]}},{quoted:md})}
     
    }
 
